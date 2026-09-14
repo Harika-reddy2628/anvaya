0c1460b3d30bc573fcfd31af01bef740d337058e
abbysallord <dshenoyh@gmail.com>
2026-09-14 18:03:27 +0530

    build: configure dependencies, Tailwind CSS v4, and Next.js 16 tooling

---
+++ src/types/components.d.ts
@@ -0,0 +1,17 @@
+declare module "@/components/Scanner" {
+  import React from "react";
+  const Scanner: React.FC<any>;
+  export default Scanner;
+}
+
+declare module "@/components/Prism" {
+  import React from "react";
+  const Prism: React.FC<any>;
+  export default Prism;
+}
+
+declare module "@/components/Grainient" {
+  import React from "react";
+  const Grainient: React.FC<any>;
+  export default Grainient;
+}
